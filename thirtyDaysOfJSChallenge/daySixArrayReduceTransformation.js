d350c17029b5972c89afe1719f772d5f1bd28a76
MakWils <kaylaanngrace@gmail.com>
2023-05-10 11:53:52 -0500

    day six of js challenge

---
+++ thirtyDaysOfJSChallenge/daySixArrayReduceTransformation.js
@@ -0,0 +1,43 @@
+/* 
+Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
+
+A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+
+If the length of the array is 0, it should return init.
+
+Please solve it without using the built-in Array.reduce method.
+*/
+
+// BRUTE FORCE 
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let results 
+    if (nums.length === 0) {
+        return init
+    } else {
+        for (let i = 0; i < nums.length; i++) {
+            results = fn(init, nums[i])
+            init = results
+        }
+        return results
+    }
+};
+
+// REFACTORED
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let results = init
+    nums.forEach(n => results = fn(results, n))
+    return results
+};
