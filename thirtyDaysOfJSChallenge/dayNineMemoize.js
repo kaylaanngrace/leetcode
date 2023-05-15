cc2dcd81f64ed16d91ca5665e1f2264daecfee69
MakWils <kaylaanngrace@gmail.com>
2023-05-15 15:57:01 -0500

    day nine of javascript challenge

---
+++ thirtyDaysOfJSChallenge/dayNineMemoize.js
@@ -0,0 +1,53 @@
+/*
+Given a function fn, return a memoized version of that function.
+
+A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+You can assume there are 3 possible input functions: sum, fib, and factorial.
+
+sum accepts two integers a and b and returns a + b.
+fib accepts a single integer n and returns 1 if n <= 1 or fib(n - 1) + fib(n - 2) otherwise.
+factorial accepts a single integer n and returns 1 if n <= 1 or factorial(n - 1) * n otherwise.
+*/
+
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if(key in cache) {
+            return cache[key]
+        }
+        const funcOutput = fn(...args);
+        cache[key] = funcOutput;
+        return funcOutput;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
+
+/*
+Complexity Analysis
+The following analysis applies to all approaches.
+
+Let NNN be the number of times the function was called previously.
+
+Time complexity: O(1)O(1)O(1). Each time the memoized function is called, a single dictionary lookup is performed.
+
+Space complexity: O(N)O(N)O(N). In the worst case, all the previous arguments need to be stored.
+
+
+
+*/
